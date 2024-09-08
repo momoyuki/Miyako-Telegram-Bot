47cc4cbe9de272ad0a162026d58fe1f41722ccb9
megumin <6390150+momoyuki@users.noreply.github.com>
2024-09-08 21:44:10 +0700

    ปรับแก้บัค เงื่อนไขเฉพาะ

--- miyako.py
+++ miyako.py
@@ -23,20 +23,12 @@ file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelnam
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
-# ฟังก์ชันที่จะรันเมื่อผู้ใช้พิมพ์ /start
-async def start(update: Update, context: CallbackContext):
-    await update.message.reply_text('RABBIT 1 พร้อมออกปฏิบัติการทุกเมื่อค่ะ')
 
-def clear_whitespace(text):
-    return re.sub(r'\s+', '', text)
-
-# ฟังก์ชันซ่อมลิงก์
 def fixup_link(text):
     # ลบข้อความหน้าลิงก์ที่เริ่มต้นด้วย 'http' เช่น SChttps://...
     text = re.sub(r'\S*?(https?://\S+)', r'\1', text)
     # ลบช่องว่างระหว่าง protocol และ domain
     text = re.sub(r'(https?://)\s+', r'\1', text)
-    text = re.sub(r'(https?://)\s*([a-zA-Z0-9.-]+)\s*\.\s*([a-zA-Z]{2,})', r'\1\2.\3', text)
 
     # ตรวจสอบว่ามีลิงก์ fixupx.com หรือ fxtwitter.com แล้วหรือไม่
     if 'fixupx.com' in text or 'fxtwitter.com' in text:
@@ -47,40 +39,48 @@ def fixup_link(text):
     text = re.sub(r'www\s*\.\s*twitter\s*\.\s*com\s*/\s*', r'www.twitter.com/', text, flags=re.IGNORECASE)
     text = re.sub(r'www\s*\.\s*x\s*\.\s*com\s*/\s*', r'www.x.com/', text, flags=re.IGNORECASE)
     
+
     # ซ่อมลิงก์ pixiv.net
     text = re.sub(r'pixiv\s*\.\s*net\s*/\s*', r'pixiv.net/', text, flags=re.IGNORECASE)
     text = re.sub(r'www\s*\.\s*pixiv\s*\.\s*net\s*/\s*', r'www.pixiv.net/', text, flags=re.IGNORECASE)
-    text = re.sub(r'(artworks)\s*[-:/|\s]+\s*(\d+)',handle_artwork,text)
+    
+    
 
     # ซ่อมลิงก์ที่เกี่ยวข้องกับ artworks
     text = re.sub(r"(https?://)?(www\.)?pixiv\.net/en/artworks/(\d+)", r"https://www.pixiv.net/en/artworks/\3", text, flags=re.IGNORECASE)
 
-    text = re.sub(r'(\w+)\s*/?\s*(status)\s*/?\s*(\d+)',handle_tx,text)
-
-    # ทำเป็นอีกฟังก์ชั่น
     # แทนที่ลิงก์ x.com และ twitter.com ด้วย fixupx.com และ fxtwitter.com
     text = re.sub(r'(https?://)?(x\.com)', r'\1fixupx.com', text)
     text = re.sub(r'(https?://)?(twitter\.com)', r'\1fxtwitter.com', text)
-    # จัดการกับ @username แก้โดยดัก @ถ้าเจอ แทนที่ด้วยลิงก์
-    
-    text = re.sub(r'@(\w+)', handle_username, text)
+
+    # จัดการกับ @username
     text = re.sub(r'(x|twitter|@)(\s*[-:|\s]+\s*)(\w+)', handle_username_tx, text)
-
-   
-    # text = re.sub(r'(?:@|X|Twitter)\s*:\s*(\w+)', handle_username, text, flags=re.IGNORECASE)
-
+    text = re.sub(r'@(\w+)', handle_username, text)
+    
     return text
 
+def only_plain_text(text):
+    #TODO แก้บัค กรณี หลุดเซ็ตอื่น
+    text = re.sub(r'(artworks)\s*[-:/|\s]+\s*(\d+)', handle_artwork, text) 
+    text = re.sub(r'(\w+)\s*/?\s*(status)\s*/?\s*(\d+)', handle_tx, text)
+    return text
+
+# ฟังก์ชันลบช่องว่าง
+def clear_whitespace(text):
+    return re.sub(r'\s+', '', text)
+
+# จัดการกับ Pixiv artworks #TODO แก้บัค กรณี หลุดเซ็ตอื่น
 def handle_artwork(match):
     artworks_id = match.group(2).strip()
     return f"https://www.pixiv.net/en/artworks/{artworks_id}"
 
+# จัดการกับ Twitter และ X status #TODO แก้บัค กรณี หลุดเซ็ตอื่น
 def handle_tx(match):
     username = match.group(1).strip()
     status_id = match.group(3).strip()
     return f"@{username} https://x.com/{username}/status/{status_id}"
 
- # จัดการกับ @username
+# จัดการกับ @username บนแพลตฟอร์ม Twitter หรือ X
 def handle_username_tx(match):
     username = match.group(3).strip()
     if username.lower() == BOT_NAME:
@@ -88,6 +88,7 @@ def handle_username_tx(match):
     platform = random.choice(["x", "twitter"])
     return f"@{username} [{platform}.com/{username}]"
 
+# จัดการกับ @username ทั่วไป
 def handle_username(match):
     username = match.group(1).strip()
     if username.lower() == BOT_NAME:
@@ -95,35 +96,49 @@ def handle_username(match):
     platform = random.choice(["x", "twitter"])
     return f"@{username} [{platform}.com/{username}]"
 
-###########################คำสั่ง###########################
+# ฟังก์ชันที่จะรันเมื่อผู้ใช้พิมพ์ /start
+async def start(update: Update, context: CallbackContext):
+    await update.message.reply_text('RABBIT 1 พร้อมออกปฏิบัติการทุกเมื่อค่ะ')
 
 async def link_command(update: Update, context: CallbackContext) -> None:
-    user_message = update.message.text[len('/link '):].strip()
-    if user_message:
-        cleaned_message = clear_whitespace(user_message)
-        await update.message.reply_text(f'เป้าหมายใหม่ค่ะ! : {cleaned_message}')
-        logger.warning(f'/link {update.effective_user.first_name} : {cleaned_message}')
-    else:
-        await update.message.reply_text('ไม่ได้ใช้แบบนี้ค่ะ /link ตามลิงก์ที่ไม่สมบูรณ์ค่ะ')
-
-async def fixup_command(update: Update, context: CallbackContext) -> None:
     if update.message and update.message.text:
         command_and_text = update.message.text.split(' ', 1)
         if len(command_and_text) > 1:
-            user_message = command_and_text[1]
-            fixed_message = fixup_link(user_message)
-            if fixed_message != user_message:
-                await update.message.reply_text(f'ตรวจพบเป้าหมายแล้วค่ะ : {fixed_message}')
-                logger.info(f'{update.effective_user.first_name} : {fixed_message}')
+            user_message = command_and_text[1].strip()  # ดึงข้อความต่อจากคำสั่ง /link
+            if user_message:
+                cleaned_message = clear_whitespace(user_message)
+                await update.message.reply_text(f'เป้าหมายใหม่ค่ะ! : {cleaned_message}')
+                logger.info(f'/link {update.effective_user.first_name} : {cleaned_message}')
             else:
-                await update.message.reply_text(f'ขอโทษค่ะแก้ไขไม่ได้จะบันทึกไว้นะคะ {update.effective_user.first_name}')
-                logger.warning(f'{update.effective_user.first_name} : {user_message}')
+                await update.message.reply_text('ไม่ได้ใช้แบบนี้ค่ะ /link ตามลิงก์ที่ไม่สมบูรณ์ค่ะ')
         else:
             await update.message.reply_text('กรุณาใส่ลิงก์ที่ต้องการซ่อมด้วยค่ะ')
     else:
         logger.warning('ข้อความว่างหรือไม่ได้รับการประมวลผล')
 
-################################################################################################
+async def fixup_command(update: Update, context: CallbackContext) -> None:
+    if update.message and update.message.text:
+        command_and_text = update.message.text.split(' ', 1)
+        if len(command_and_text) > 1:
+            user_message = command_and_text[1].strip()
+            fixed_message = fixup_link(user_message)
+            if fixed_message != user_message:
+                await update.message.reply_text(f'ตรวจพบเป้าหมายแล้วค่ะ : {fixed_message}')
+                logger.info(f'{update.effective_user.first_name} : {fixed_message}')
+            else:
+                #TODO
+                fixed_message = only_plain_text(user_message)
+                if fixed_message != user_message:
+                    await update.message.reply_text(f'ตรวจพบเป้าหมายแล้วค่ะ : {fixed_message}')
+                    logger.warning(f'ด่านสุดท้าย {update.effective_user.first_name} : {user_message}')
+                else:   
+                #TODO
+                    await update.message.reply_text(f'ขอโทษค่ะแก้ไขไม่ได้จะบันทึกไว้นะคะ {update.effective_user.first_name}')
+                    logger.warning(f'{update.effective_user.first_name} : {user_message}')
+        else:
+            await update.message.reply_text('กรุณาใส่ลิงก์ที่ต้องการซ่อมด้วยค่ะ')
+    else:
+        logger.warning('ข้อความว่างหรือไม่ได้รับการประมวลผล')
 
 async def handle_private_message(update: Update, context: CallbackContext) -> None:
     if update.message and update.message.text:
@@ -141,12 +156,29 @@ async def handle_private_message(update: Update, context: CallbackContext) -> No
 
 async def handle_group_message(update: Update, context: CallbackContext) -> None:
     if update.message and update.message.text:
-        if update.message.text.startswith(f'/fixup@{BOT_NAME}'):
-            await fixup_command(update, context)
-        elif update.message.text.startswith(f'/link@{BOT_NAME}'):
-            await link_command(update, context)
+        user_message = update.message.text
 
-################################################################################################
+        # ตรวจสอบทั้งกรณีที่มีชื่อบอทและไม่มีชื่อบอท
+        if user_message.startswith(f'/fixup@{BOT_NAME}') or user_message.startswith('/fixup'):
+            await fixup_command(update, context)
+        elif user_message.startswith(f'/link@{BOT_NAME}') or user_message.startswith('/link'):
+            await link_command(update, context)
+        else:
+            # ข้อความที่ไม่ได้ใช้คำสั่ง
+            logger.info(f'{update.effective_user.first_name} ส่งข้อความที่ไม่ใช่คำสั่ง: {user_message}')
+
+async def repot_bug_command(update: Update, context: CallbackContext):
+    if update.message and update.message.text:
+        command_and_text = update.message.text.split(' ', 1)
+        if len(command_and_text) > 1:
+            user_message = command_and_text[1].strip()
+            await update.message.reply_text(f'รับทราบแล้วค่ะจะทำเรื่องบันทึกไว้ให้นะคะ {update.effective_user.first_name}')
+            logger.warning(f'รายงานบัค :{update.effective_user.first_name} : {user_message}')
+        else:
+         await update.message.reply_text('สามารถพิมพ์ รายงานโดยการ กด /bug ตามด้วยข้อความได้เลยค่ะ')
+
+async def privacy_command(update: Update, context: CallbackContext):
+    await update.message.reply_text('ระบบจะทำการเก็บข้อมูลการใช้งานไปวิเคราะห์และปรับปรุงให้ดียิ่งขึ้นค่ะ')
 
 
 TOKEN = os.getenv('TOKEN')
@@ -165,10 +197,15 @@ app.add_handler(CommandHandler("link", link_command))
 # เพิ่มคำสั่ง /fixup ให้บอท
 app.add_handler(CommandHandler("fixup", fixup_command))
 
+app.add_handler(CommandHandler("bug", repot_bug_command))
+
+app.add_handler(CommandHandler("privacy", privacy_command))
 
 # start - เริ่มใช้งาน
 # link - ลบช่องว่างระหว่างลิงก์
-# fixup - แก้ไขลิงก์ @x.com,@twitter.com,pixiv.net  
+# fixup - แก้ไขลิงก์ @x.com,@twitter.com,pixiv.net 
+# bug - รายงานบัค
+# privacy - เกี่ยวกับนโยบายความเป็นส่วนตัว 
 
 # กำหนด handler สำหรับการแชทส่วนตัว
 app.add_handler(MessageHandler(filters.TEXT & filters.ChatType.PRIVATE, handle_private_message))
